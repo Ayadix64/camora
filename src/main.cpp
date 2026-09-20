54355b9116af33ef3fa740d8f03821a112748f78
Ayadi <ayadi.chamseddine84@gmail.com>
2026-09-20 13:08:15 +0100

    menu sikliton

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <atomic>
+#include <cmath>
+#include <cstdlib>
 #include <cstring>
 #include <pthread.h>
 #include <stdio.h>
@@ -26,7 +28,7 @@
 #define min(x,y) x>y?y:x
 #define max(x,y) x>y?x:y
 #define pos_or_zeor(x) x<0?0:x
-
+#define cube(x) (x)*(x) // yeah..
 
 void *imagebuffer = NULL;
 
@@ -47,13 +49,72 @@ void captureThread( void* buff,
         	}
                 pthread_mutex_unlock(capmutex);
 
-                //pthread_mutex_lock(buffmut);
+                pthread_mutex_lock(buffmut);
                 memcpy(buff, frame.data, frame.rows*frame.cols*3);
-                //pthread_mutex_unlock(buffmut);
+                pthread_mutex_unlock(buffmut);
         }
-
+        return;
 }
 
+bool snapButton(void)
+{
+
+        static unsigned int raduis = 30;
+        static bool isHover = false;
+        static double snaptimer = 0.0;
+        const float animationduration = 0.1;
+        bool isClicked = false;
+        float x  = GetWindowW()/2.0, y = GetWindowH() -40;
+
+        if( (cube(GetMousePos().x - x) +  cube(GetMousePos().y -y)) < 30* 30){
+                if(!isHover)snaptimer = glfwGetTime();
+                isHover=true;
+                isClicked= GetMouseClickes()&1;
+        }else if(isHover){
+                snaptimer = glfwGetTime() + animationduration;
+                isHover = false;
+        }
+        if(!isHover && glfwGetTime() >   snaptimer){
+
+                snaptimer = glfwGetTime();
+        }
+        if(abs(glfwGetTime() - snaptimer) <= animationduration && glfwGetTime() != snaptimer  )
+        {
+                raduis = 30+abs(glfwGetTime() - snaptimer)*10.0 * (1.0/animationduration);
+        }
+        if(isClicked){
+                snaptimer = glfwGetTime() + animationduration;
+                printf("clicked\n");
+        }
+
+        DrawCircle(x,y, raduis, 360, {245,245,240,255});
+        DrawEmptyCircle(x,  y, 40,5, 360, {245,245,240,255});
+        return isClicked;
+}
+
+void Menu(){
+        static bool showMenu = false;
+        const  double animationDur = 0.2f;
+        static double animationEnd = 0.0;
+        static int goUp= 0.0; //up ? down?
+        static float y = GetWindowH();
+
+        if(showMenu || animationEnd > glfwGetTime() ){
+                DrawRoundedRectangel(0 ,(float)GetWindowH()- y*(4*(float)GetWindowH()/5), GetWindowW(), GetWindowH() , 30, 36, {25,25,30,255});
+        }
+        if(glfwGetTime() >= animationEnd && animationEnd > 0.0){
+                animationEnd=0.0;
+        }else if(animationEnd>0.0){
+                y = (1.0f/animationDur)*(goUp?animationDur-(animationEnd-glfwGetTime()) : (animationEnd-glfwGetTime()));
+        }
+
+        if(Button("Menu", 20 ,GetWindowH() - 50, 0, 0)&1){
+               showMenu = !showMenu;
+               goUp= showMenu?1.0:0.0;
+               animationEnd = glfwGetTime() + animationDur;
+        }
+        //animation , privded by trust me bro fondation
+}
 
 
 int main(){
@@ -62,7 +123,7 @@ int main(){
 	}
 
 	TickInitWindowFlags();
-	GLFWwindow* window = glfwCreateWindow(800, 600, "window", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "camora", NULL, NULL);
 	glfwMakeContextCurrent(window);//seting our widow context
 	gladLoadGL();//Init our opengl her, this will get all the function adrress of opengl to used;
 
@@ -105,13 +166,13 @@ int main(){
 	TickTexture2D videotexture = LoadTexture(frame.data, frame.cols, frame.rows, 3);
 
 
-	std::thread(captureThread,buff,&buffmut ,&cap, &capmutex, &sholdclose).detach();
+	std::thread capthr= std::thread(captureThread,buff,&buffmut ,&cap, &capmutex, &sholdclose);
 
 	while(!glfwWindowShouldClose(window) ){
 		TickNewFrame();//you have to call this in evry frime; or you will have a bed time!
-		//pthread_mutex_lock(&buffmut);
+		pthread_mutex_lock(&buffmut);
 		ReloadTexture(&videotexture, buff, frame.cols, frame.rows, 3);
-		//pthread_mutex_unlock(&buffmut);
+		pthread_mutex_unlock(&buffmut);
 		float frameHdem = (float)frame.cols/(float)frame.rows;
 		float w = min((float)GetWindowW(),(float)GetWindowH()*frameHdem);
 		float h = min(w/frameHdem,GetWindowW());
@@ -120,16 +181,20 @@ int main(){
 					w,
 					h
 		);
-		/******************** Render ********************/
-		DrawCircle(GetWindowW() / 2,GetWindowH() - 40, 30, 360, {245,245,240,255});
-		TickRendre(); /*render our Tick data*/
+		snapButton();
+		Menu();
 
+		TickRendre();
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //clering up our screen
 		tflf=glfwGetTime () ;
 	}
-	//sholdclose=true;
+	sholdclose=true;
+	if(capthr.joinable()){
+	        capthr.join();
+	}
 	free(buff);
 	glfwTerminate();
+	return 0;
 }
